adc7bfe122432cc9764f022b8f89f013b148bea0
JairoCruz <azaveliz@gmail.com>
2018-09-17 10:12:00 -0600

    Seccion:2-vid:6 Handling onSubmit and saving to firebase realtime database

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,79 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import { database } from "../firebase";
 
 class App extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
     // state
     this.state = {
-      title: '',
-      body: ''
-    }
+      title: "",
+      body: ""
+    };
     // bind
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  // handle change
   handleChange(e) {
     this.setState({
       [e.target.name]: e.target.value
     });
   }
 
+  // handle submit
+  handleSubmit(e){
+    // avoid page reload
+    e.preventDefault();
+    const note = {
+      title: this.state.title,
+      body: this.state.body
+    }
+    // save object note in database firebase
+    database.push(note);
+    // reset state
+    this.setState({
+      title: '',
+      body: ''
+    });
+  }
+
   render() {
     return (
       <div className="container-fluid">
-      <div className="row">
-      <div className="col-sm-6 offset-sm-3">
-        <form>
-          <div className="form-group">
-            <input onChange={ this.handleChange } type="text" name="title" className="form-control no-border" placeholder="Title..." required/>
-          </div>
+        <div className="row">
+          <div className="col-sm-6 offset-sm-3">
+            <form onSubmit={ this.handleSubmit }>
+              <div className="form-group">
+                <input
+                  onChange={this.handleChange}
+                  value={ this.state.title }
+                  type="text"
+                  name="title"
+                  className="form-control no-border"
+                  placeholder="Title..."
+                  required
+                />
+              </div>
 
-          <div className="form-group">
-            <textarea onChange={ this.handleChange } type="text" name="body" className="form-control no-border" placeholder="Body..." required/>
-          </div>
+              <div className="form-group">
+                <textarea
+                  onChange={this.handleChange}
+                  value={ this.state.body }
+                  type="text"
+                  name="body"
+                  className="form-control no-border"
+                  placeholder="Body..."
+                  required
+                />
+              </div>
 
-          <div className="form-group">
-            <button className="btn btn-primary col-sm-12">Save</button>
+              <div className="form-group">
+                <button className="btn btn-primary col-sm-12">Save</button>
+              </div>
+            </form>
           </div>
-        </form>
-      </div>
-      </div>       
+        </div>
       </div>
     );
   }
