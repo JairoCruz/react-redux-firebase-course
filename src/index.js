d04a7ca295411d207882a5b360f2caf7a9ae6a18
JairoCruz <azaveliz@gmail.com>
2018-09-17 11:36:15 -0600

    Seccion:3-vid:9 Creating redux store, there is'nt vid:8, only document read

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './style/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+// redux
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+// create redux store -> reducers -> 'actions' | applyMiddleware()
+const store = createStore(composeWithDevTools());
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
