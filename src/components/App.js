8823b88994761beb7617997c3ffe24cc215d7120
JairoCruz <azaveliz@gmail.com>
2018-09-17 09:27:14 -0600

    Seccion:2-vid:5 Handling onChange event

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,24 @@ import React, { Component } from 'react';
 
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    // state
+    this.state = {
+      title: '',
+      body: ''
+    }
+    // bind
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
   render() {
     return (
       <div className="container-fluid">
@@ -9,11 +27,11 @@ class App extends Component {
       <div className="col-sm-6 offset-sm-3">
         <form>
           <div className="form-group">
-            <input type="text" name="title" className="form-control no-border" placeholder="Title..." required/>
+            <input onChange={ this.handleChange } type="text" name="title" className="form-control no-border" placeholder="Title..." required/>
           </div>
 
           <div className="form-group">
-            <textarea type="text" name="body" className="form-control no-border" placeholder="Body..." required/>
+            <textarea onChange={ this.handleChange } type="text" name="body" className="form-control no-border" placeholder="Body..." required/>
           </div>
 
           <div className="form-group">
