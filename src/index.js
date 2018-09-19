530a1a6dbc4f15b2b169f9d4d3f6c7bb6f3a1e63
JairoCruz <azaveliz@gmail.com>
2018-09-19 14:05:56 -0600

    Seccion:6-vid:19 Building navbar

--- src/index.js
+++ src/index.js
@@ -6,15 +6,52 @@ import registerServiceWorker from "./registerServiceWorker";
 // redux
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-import thunk from 'redux-thunk';
+import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./reducers";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import Login from "./components/Login";
 // create redux store -> reducers -> 'actions - actionType' | applyMiddleware()
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+// Header Navigation
+const Header = () => (
+  <nav className="navbar navbar-default">
+    <div className="container-fluid">
+      <div className="navbar-header">
+        <button
+          type="button"
+          className="navbar-toggle"
+          data-toggle="collapse"
+          data-target="#myNavbar"
+        >
+          <span className="icon-bar" />
+          <span className="icon-bar" />
+          <span className="icon-bar" />
+        </button>
+
+        <Link className="navbar-brand" to="/">
+          DIARY2018
+        </Link>
+      </div>
+    </div>
+  </nav>
+);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/" component={App} exact={true} />
+          <Route path="/login" component={Login} exact={true} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
