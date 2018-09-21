14aa441bdc6b6b564b0b1b19c7614ba781ba93b2
JairoCruz <azaveliz@gmail.com>
2018-09-21 15:31:10 -0600

    Seccion:8-vid:26 Moving routes

--- src/index.js
+++ src/index.js
@@ -11,41 +11,11 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./reducers";
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import Login from "./components/Login";
+import Header from './routes/Header';
 // create redux store -> reducers -> 'actions - actionType' | applyMiddleware()
 const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(thunk))
-);
-
-// Header Navigation
-const Header = () => (
-  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div className="container-fluid">
-      <div className="navbar-header">
-        <button
-          type="button"
-          className="navbar-toggler"
-          data-toggle="collapse"
-          data-target="#myNavbar"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-
-        <Link className="navbar-brand" to="/">
-          DIARY2018
-        </Link>
-      </div>
-      <div className="collapse navbar-collapse" id="myNavbar">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item active">
-            <Link className="nav-link" to="/login">
-              Login
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
 );
 
 ReactDOM.render(
---
+++ src/routes/Header.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { getUser, logout } from "../actions/userAction";
+
+class Header extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <button
+              type="button"
+              className="navbar-toggler"
+              data-toggle="collapse"
+              data-target="#myNavbar"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+
+            <Link className="navbar-brand" to="/">
+              DIARY2018
+            </Link>
+          </div>
+          <div className="collapse navbar-collapse" id="myNavbar">
+            <ul className="navbar-nav ml-auto">
+              {
+                  this.props.user === null ? (
+                    <li className="nav-item active">
+                    <Link className="nav-link" to="/login">
+                      Login
+                    </Link>
+                  </li>
+                  ) : (
+                      <li>
+                          <Link to="/logout">logout</Link>
+                      </li>
+                  )
+              }
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+function mapStateToProps(state, ownProps) {
+  return {
+    user: state.user
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  { getUser, logout }
+)(Header);
