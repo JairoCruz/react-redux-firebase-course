ddf34a1161ee91186d2b494a726badc5826eb7a0
JairoCruz <azaveliz@gmail.com>
2018-09-25 10:10:51 -0600

    Seccion:9-vid:28 Dispatching notes status;vid:29 Dispatching user status; Seccion:10-vid:30 Loading reducer

--- src/actions/notesAction.js
+++ src/actions/notesAction.js
@@ -1,21 +1,37 @@
-import { GET_NOTES } from "../actionTypes";
+import { GET_NOTES, NOTES_STATUS } from "../actionTypes";
 import { database } from "../firebase";
 
-export function getNotes(){
-    return dispatch => {
-        database.on('value', snapshot =>{
-            dispatch({
-                type: GET_NOTES,
-                payload: snapshot.val()
-            });
-        });
-    };
+export function getNotes() {
+  return dispatch => {
+    // as soon as this functions fires show loading to true
+    dispatch({
+      type: NOTES_STATUS,
+      payload: true
+    });
+    database.on("value", snapshot => {
+      dispatch({
+        type: GET_NOTES,
+        payload: snapshot.val()
+      });
+      // once notes are received show loading to false
+      dispatch({
+        type: NOTES_STATUS,
+        payload: false
+      });
+      // wait until something changes and try again
+    }, () => {
+        dispatch({
+            type: NOTES_STATUS,
+            payload: -1
+        })
+    });
+  };
 }
 
-export function saveNote(note){
-    return dispatch => database.push(note);
+export function saveNote(note) {
+  return dispatch => database.push(note);
 }
 
-export function deleteNote(id){
-    return dispatch => database.child(id).remove();
+export function deleteNote(id) {
+  return dispatch => database.child(id).remove();
 }
