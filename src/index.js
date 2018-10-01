82f04029b23850fd85b12bd8123f9b8579414acb
JairoCruz <azaveliz@gmail.com>
2018-10-01 15:55:36 -0600

    Seccion:14-vid:46 Redirect to login page

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./reducers";
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Link, Redirect } from "react-router-dom";
 import Login from "./components/Login";
 import Header from "./routes/Header";
 import LoadingComponent from "./components/LoadingComponent";
@@ -29,6 +29,7 @@ ReactDOM.render(
         <div>
           <Switch>
             <Route path="/login" component={Login} exact={true} />
+            <Redirect from="/logout" to="/login" />
             <AuthenticatedComponent>
               <Header />
               <Route path="/" component={App} exact={true} />
