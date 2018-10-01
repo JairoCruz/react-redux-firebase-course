fcdd0dd75984c732fdd365e075c4f51c7eeb5c1b
JairoCruz <azaveliz@gmail.com>
2018-10-01 13:53:44 -0600

    Seccion:12-vid:42 Rendering comments

---
+++ src/components/Comment.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+/*const Comment = (props) => {
+    return(<div>{props.children}</div>);
+}*/
+const Comment = ({ children }) => <div> {children}</div>;
+
+export default Comment;
